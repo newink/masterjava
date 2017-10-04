1bb12093f96b13ffd46ecddbf59d977d49962e43
Sergey Ivanov <newinkwell@yandex.ru>
2017-10-04 11:03:07 +0500

    MailExecution with CompletionService

--- src/main/java/ru/javaops/masterjava/service/MailService.java
+++ src/main/java/ru/javaops/masterjava/service/MailService.java
@@ -1,8 +1,10 @@
 package ru.javaops.masterjava.service;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import com.sun.org.apache.xerces.internal.xs.StringList;
+
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 public class MailService {
     private static final String OK = "OK";
@@ -11,8 +13,55 @@ public class MailService {
     private static final String INTERRUPTED_BY_TIMEOUT = "+++ Interrupted by timeout";
     private static final String INTERRUPTED_EXCEPTION = "+++ InterruptedException";
 
+    private final ExecutorService executorService = Executors.newFixedThreadPool(8);
+
     public GroupResult sendToList(final String template, final Set<String> emails) throws Exception {
-        return new GroupResult(0, Collections.emptyList(), null);
+        CompletionService<MailResult> completionService = new ExecutorCompletionService<>(executorService);
+
+        List<Future<MailResult>> futures = emails.stream()
+                .map(email -> completionService.submit(() -> sendToUser(template, email)))
+                .collect(Collectors.toList());
+
+        return new Callable<GroupResult>() {
+            private int succeeded;
+            private List<MailResult> failedMails = new ArrayList<>();
+
+            @Override
+            public GroupResult call() {
+
+                while (!futures.isEmpty()) {
+                    try {
+                        Future<MailResult> future = completionService.poll(10, TimeUnit.SECONDS);
+                        if (future == null) {
+                            return cancelWithFail(INTERRUPTED_BY_TIMEOUT);
+                        }
+
+                        MailResult mailResult = future.get();
+                        futures.remove(future);
+
+                        if (mailResult.isOk()) {
+                            succeeded++;
+                        } else {
+                            failedMails.add(mailResult);
+                            if (failedMails.size() >= 5) {
+                                return cancelWithFail(INTERRUPTED_BY_FAULTS_NUMBER);
+                            }
+                        }
+                    } catch (InterruptedException e) {
+                        return cancelWithFail(INTERRUPTED_EXCEPTION);
+                    } catch (ExecutionException e) {
+                        return cancelWithFail(e.getCause().getMessage());
+                    }
+                }
+
+                return new GroupResult(succeeded, failedMails, null);
+            }
+
+            private GroupResult cancelWithFail(String cause) {
+                futures.forEach(mailResultFuture -> mailResultFuture.cancel(true));
+                return new GroupResult(succeeded, failedMails, cause);
+            }
+        }.call();
     }
 
 
@@ -70,6 +119,16 @@ public class MailService {
             return "Success: " + success + '\n' +
                     "Failed: " + failed.toString() + '\n' +
                     (failedCause == null ? "" : "Failed cause" + failedCause);
+        }
+    }
+
+    public static void main(String[] args) {
+        MailService service = new MailService();
+        try {
+            GroupResult groupResult = service.sendToList("template", new HashSet<>(Arrays.asList("kyuek@email.ru", "nghjull@email.ru", "zejhkro@email.ru","kuyek@email.ru", "nuldfgl@email.ru", "zedfgro@email.ru","kfgek@email.ru", "nulasdl@email.ru", "zerdfgo@email.ru","kgfek@email.ru", "nulasdl@email.ru", "zeasdro@email.ru","kasdek@email.ru", "nulasdl@email.ru", "zeasdro@email.ru")));
+            System.out.println(groupResult);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
