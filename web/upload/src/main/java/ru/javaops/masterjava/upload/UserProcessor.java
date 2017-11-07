c45a915c4a82c2d7a01e1940c2ba5b7c9fcb398e
Sergey Ivanov <iss@pkzdrav.ru>
2017-11-07 15:05:16 +0500

    4 3 HW3 upload servlet3

--- web/upload/src/main/java/ru/javaops/masterjava/upload/UserProcessor.java
+++ web/upload/src/main/java/ru/javaops/masterjava/upload/UserProcessor.java
@@ -1,9 +1,10 @@
 package ru.javaops.masterjava.upload;
 
 import ru.javaops.masterjava.model.User;
-import ru.javaops.masterjava.model.UserFlag;
+import ru.javaops.masterjava.xml.util.JaxbParser;
 import ru.javaops.masterjava.xml.util.StaxStreamProcessor;
 
+import javax.xml.bind.JAXBException;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.XMLEvent;
 import java.io.InputStream;
@@ -11,16 +12,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserProcessor {
+    private JaxbParser parser = new JaxbParser(User.class);  // model.User
 
-    public List<User> process(final InputStream is) throws XMLStreamException {
+    public List<User> process(final InputStream is) throws XMLStreamException, JAXBException {
         final StaxStreamProcessor processor = new StaxStreamProcessor(is);
         List<User> users = new ArrayList<>();
 
         while (processor.doUntil(XMLEvent.START_ELEMENT, "User")) {
-            final String email = processor.getAttribute("email");
-            final UserFlag flag = UserFlag.valueOf(processor.getAttribute("flag"));
-            final String fullName = processor.getReader().getElementText();
-            final User user = new User(fullName, email, flag);
+            User user = parser.unmarshal(processor.getReader(), User.class);
             users.add(user);
         }
         return users;
